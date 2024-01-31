3a49cbc005bd820d9c1d22bba839991858395dad
lokeshsharma19 <lokeshsharmacp2023@gmail.com>
2024-01-31 22:55:42 +0530

    deployed

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,6 +1,13 @@
 function Heading() {
   return (
-    <div style={{ display: "inline-block" }}>
+    <div
+      style={{
+        margin: "1rem",
+        padding: "1rem",
+        borderRadius: "0.4rem",
+        backgroundColor: "lightgray",
+        display: "inline-block",
+      }}>
       <h2>Book my show</h2>
     </div>
   );
